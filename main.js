69e768e1d4abd878cdaf577031e4063c1ea65b7b
Shubham Sharma <shubham@iastate.edu>
2019-05-30 15:42:14 -0500

    Cleaning up unused code

--- main.js
+++ main.js
@@ -1,22 +1,82 @@
-var app = require('electron').remote;
-var dialog = app.dialog;
-var fs = require('fs');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
 
-document.getElementById('createButton').onclick = () =>  {
-    dialog.showSaveDialog( (fileName) => {
-        if(fileName === undefined){
-            alert("You didn't save the file");
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('mainWindow.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+
+  let dir;
+
+
+  ipcMain.on('selectDirectory',function () {
+    dialog.showOpenDialog((fileNames) => {
+        // fileNames is an array that contains all the selected
+        if(fileNames === undefined){
+            console.log("No file selected");
             return;
         }
 
-        var content = document.getElementById('content').value;
-
-        fs.writeFile(fileName, content, (err) => {
+        fs.readFile(fileNames[0], 'utf-8', (err, data) => {
             if(err){
-                console.log(err);
-            } else{
-                alert("The file has been successfully saved.");
+                alert("An error ocurred reading the file :" + err.message);
+                return;
             }
-        })
+    
+            // Change how to handle the file content
+            console.log("The file content is : " + data);
+
+            document.getElementById("form-control").value = data;
+        });
     });
-};
+  });
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
